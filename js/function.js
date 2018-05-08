5197730ff1ef1d9303f3a06d8c9cbb21efe61a61
zzAutumn <30762123+zzAutumn@users.noreply.github.com>
2018-05-08 10:09:32 +0800

    js function elegant writting way

--- js/function.js
+++ js/function.js
@@ -1,1 +1,37 @@
 
+/*
+* Record some elegant function code
+*
+/
+
+//ABSTRACTING REPETITION
+////////// normal way ///////
+for (let i = 0; i < 10; i++){
+  console.log(i);
+}
+
+function repeatLog(n){
+  for (let i = 0; i < n; i++){
+    console.log(i);
+  }
+}
+
+function repeat(n,action){
+  for (let i = 0; i < n; i++){
+    action(i);
+  }
+}
+
+repeat(3,console.log);
+// -> 0
+// -> 1
+// -> 2
+
+/////////elegant way /////////
+
+let labels = [];
+repeat(3, i => {
+  labels.push(`Unit ${i + 1}`);
+});
+
+// -> ['Unit 1','Unit 2','Unit 3']
